d24d7cdf64eade1491120d1070c329c85f027c9c
Yue0925 <mellet1997@outlook.com>
2023-03-30 01:08:31 +0200

    maj

--- experiments/instances/MaxCutalea/run.sh
+++ experiments/instances/MaxCutalea/run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-for file in ./instances/*; do
-    echo "$file"
-    julia vOptMaxCut.jl "$file" epsilon
-done
-
-# methodes=("bb" "bc_rootRelax" "bc_rootRelaxCPEPB" "bc_rootRelaxCP" "bc_rootRelaxEPB" "bb_EPB" "bc_EPB" "bc")
-
-# # methodes=("bb" "bc_rootRelax")
-
 # for file in ./instances/*; do
-#     for mthd in ${methodes[@]}; do
-#         echo "$file ... " $mthd
-#         julia vOptMaxCut.jl "$file" $mthd
-#     done
+#     echo "$file"
+#     julia vOptMaxCut.jl "$file" epsilon
 # done
+
+methodes=("bb" "bc_rootRelax" "bc_rootRelaxCPEPB" "bc_rootRelaxCP" "bc_rootRelaxEPB" "bb_EPB" "bc_EPB" "bc")
+
+# methodes=("bb" "bc_rootRelax")
+
+for file in ./instances/*; do
+    for mthd in ${methodes[@]}; do
+        echo "$file ... " $mthd
+        julia vOptMaxCut.jl "$file" $mthd
+    done
+done
