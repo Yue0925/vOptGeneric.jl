17eb3b1e9a0c2c2672debba8816e49cca54caa2a
Yue0925 <mellet1997@outlook.com>
2023-04-21 19:02:11 +0200

    inter

--- src/BO01BB/LBSwithIPsolver.jl
+++ src/BO01BB/LBSwithIPsolver.jl
@@ -20,11 +20,12 @@ function callback_noCuts(cb_data)
     node_statut = callback_node_status(cb_data, model)
      
     # if node_statut == MOI.CALLBACK_NODE_STATUS_FRACTIONAL
-        tmp = callback_value.(cb_data, varArray)
-        val = tmp'* C[1, 2:end] + C[1, 1] + tmp'* C[2, 2:end] + C[2, 1]
-        if val > bst_val
-            bst_val = val ; x_star = callback_value.(cb_data, varArray)
-        end
+        # tmp = callback_value.(cb_data, varArray)
+        # val = (tmp'* C[1, 2:end] + C[1, 1]) * curr_λ[1] + (tmp'* C[2, 2:end] + C[2, 1] ) *curr_λ[2]
+        # if val > bst_val
+        #     bst_val = val ; x_star = callback_value.(cb_data, varArray)
+        # end
+        x_star = callback_value.(cb_data, varArray)
     # end
 end
 
@@ -54,7 +55,7 @@ end
 """
 New correction iterative algorithm for LBS 
     
-    #todo : should return the new lbs directly 
+    #todo : should return the new lbs directly !!!
 """
 function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; args...)
     global varArray
@@ -69,7 +70,7 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
     f1, f2 = vd.objs
     f1Sense, f2Sense = vd.objSenses
     varArray = JuMP.all_variables(m)
-    Gap = 0.0
+    # Gap = 0.0 # todo : useless 
 
     varArray_copied = JuMP.all_variables(lp_copied)
     f1_copied = varArray_copied'* c[1, 2:end] + c[1, 1]
@@ -79,9 +80,8 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
 
     R1 = f1Sense==MOI.MIN_SENSE ? (<=) : (>=)
     R2 = f2Sense==MOI.MIN_SENSE ? (<=) : (>=)
-    weak_dom(a, b) = R1(a[1], b[1]) && R2(a[2], b[2]) && (a[1]!= b[1] || a[2]!= b[2]) # todo : useless
+    # weak_dom(a, b) = R1(a[1], b[1]) && R2(a[2], b[2]) && (a[1]!= b[1] || a[2]!= b[2]) # todo : useless
     
-
     # set up callback 
     MOI.set(m, MOI.NumberOfThreads(), 1) ; MOI.set(m, MOI.UserCutCallback(), callback_noCuts)
 
@@ -98,7 +98,7 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
     # in case of infeasibility 
     yr_1 = 0.0 ; yr_2 = 0.0 
     if status == MOI.INFEASIBLE 
-        return Y_integer, X_integer, Gap #todo : vd.Y_N empty update
+        return Y_integer, X_integer #todo : vd.Y_N empty update , Gap
     end
 
     # in case of optimality 
@@ -108,10 +108,7 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
         append!(Y_integer, Y) ; append!(X_integer, X)
 
         yr_1 = JuMP.value(f1) ; yr_2 = JuMP.value(f2)
-        # store results in vOptData # todo : use natural order list 
-        # push!(vd.Y_N, round_results ? round.([yr_1, yr_2]) : [yr_1, yr_2])
-        # push!(vd.X_E, JuMP.value.(varArray))
-        # push!(vd.lambda, curr_λ)
+
         idx = push!(L.natural_order_vect, Solution(JuMP.value.(varArray), [yr_1, yr_2], curr_λ ))
         updateCT(L.natural_order_vect.sols[idx])
 
@@ -137,9 +134,6 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
 
         yr_1 = x_star'* c[1, 2:end] + c[1, 1]
         yr_2 = x_star'* c[2, 2:end] + c[2, 1]
-        # store results in vOptData # todo : use natural order list 
-        # push!(vd.Y_N, round_results ? round.([yr_1, yr_2]) : [yr_1, yr_2])
-        # push!(vd.X_E, x_star) ; push!(vd.lambda, curr_λ)
 
         idx = push!(L.natural_order_vect, Solution(x_star, [yr_1, yr_2], curr_λ ))
         updateCT(L.natural_order_vect.sols[idx])
@@ -160,7 +154,7 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
 
     ys_1 = 0.0 ; ys_2 = 0.0 
     if status == MOI.INFEASIBLE
-        return Y_integer, X_integer, Gap #todo : vd.Y_N empty update 
+        return Y_integer, X_integer #todo : vd.Y_N empty update, Gap 
     end
 
     if status == MOI.OPTIMAL
@@ -169,16 +163,12 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
         append!(Y_integer, Y) ; append!(X_integer, X)
         ys_1 = JuMP.value(f1) ; ys_2 = JuMP.value(f2)
 
-        #Store results in vOptData # todo : use natural order list 
-        # push!(vd.Y_N, round_results ? round.([ys_1, ys_2]) : [ys_1, ys_2])
-        # push!(vd.X_E, JuMP.value.(varArray))
-        # push!(vd.lambda, curr_λ)
         idx = push!(L.natural_order_vect, Solution(JuMP.value.(varArray), [ys_1, ys_2], curr_λ ), filtered=true)
         idx > 0 ? updateCT(L.natural_order_vect.sols[idx]) : nothing
 
         if isapprox(yr_1, ys_1, atol=1e-3) || isapprox(yr_2, ys_2, atol=1e-3)
             # # todo : stop 
-            return Y_integer, X_integer, Gap #todo : vd.Y_N empty
+            return Y_integer, X_integer #todo : vd.Y_N empty, Gap
         end
 
     elseif status == MOI.NODE_LIMIT || status == TIME_LIMIT
@@ -204,16 +194,12 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
         ys_1 = x_star'* c[1, 2:end] + c[1, 1]
         ys_2 = x_star'* c[2, 2:end] + c[2, 1]
 
-        # store results in vOptData # todo : use natural order list 
-        # push!(vd.Y_N, round_results ? round.([ys_1, ys_2]) : [ys_1, ys_2])
-        # push!(vd.X_E, x_star)
-        # push!(vd.lambda, curr_λ)
         idx = push!(L.natural_order_vect, Solution(x_star, [ys_1, ys_2], curr_λ ), filtered=true)
         idx > 0 ? updateCT(L.natural_order_vect.sols[idx]) : nothing
 
         if isapprox(yr_1, ys_1, atol=1e-3) || isapprox(yr_2, ys_2, atol=1e-3)
             # # todo : stop 
-            return Y_integer, X_integer, Gap #todo : vd.Y_N empty
+            return Y_integer, X_integer #todo : vd.Y_N empty, Gap
         end
     else
         println("has primal ? $(JuMP.has_values(m))")
@@ -223,32 +209,46 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
     # -----------------------------------------
     # step 3 : fix the next search direction 
     # -----------------------------------------
-    if length(L.natural_order_vect.sols) < 2 
-        return Y_integer, X_integer, Gap #todo : vd.Y_N empty
+    function filtering(lb::Float64)
+        # remove all points under current line 
+        to_delete = Int64[] ; i = 1
+        for s in L.natural_order_vect.sols
+            if s.y[1]*λ[1] + s.y[2]*λ[2] < lb - 1e-4 push!(to_delete, i) end
+            i += 1
+        end
+
+        deleteat!(L.natural_order_vect.sols, to_delete)
     end
 
-    todo = Vector{Vector{Float64}}(); push!(todo, [abs(L.natural_order_vect.sols[1].λ[2] - L.natural_order_vect.sols[end].λ[2]),
-            abs(L.natural_order_vect.sols[1].λ[1] - L.natural_order_vect.sols[end].λ[1])] )
 
-    l = 1 ; r = 2
+    if length(L.natural_order_vect.sols) < 2 
+        return Y_integer, X_integer #todo : vd.Y_N empty, Gap
+    end
+
+    todo = []; 
+    
+    push!(todo, [L.natural_order_vect.sols[1].y, L.natural_order_vect.sols[end].y] )
+
     # ----------------------------------------------------------------
     # repeat the same procedure until no more direction in todo list 
     # ----------------------------------------------------------------
     while length(todo) > 0
-        λ = popfirst!(todo)
+        [yl, yr] = popfirst!(todo)
+        λ = [ abs(yr[2] - yl[2]) , abs(yl[1] - yr[1]) ] 
 
         # solve the mono scalarization problem 
         f = AffExpr(0.0)    
-        lb = λ[1]*L.natural_order_vect.sols[l].y[1] + λ[2]*L.natural_order_vect.sols[l].y[2]  
+        lb = λ[1] * yl[1] + λ[2] * yl[2]  
         JuMP.set_objective(m, f1Sense, λ[1]*f1 + λ[2]*f2) ; JuMP.set_objective(lp_copied, f1Sense, λ[1]*f1_copied + λ[2]*f2_copied)
         verbose && println("solving for $(λ[1])*f1 + $(λ[2])*f2")    
         f = λ[1]*f1 + λ[2]*f2
 
         x_star = [] ; bst_val = -Inf 
-        curr_λ = [λ[1], λ[2]]
+        curr_λ = λ
         JuMP.optimize!(m, ignore_optimize_hook=true) ; status = JuMP.termination_status(m)
     
         yt_1 = 0.0 ; yt_2 = 0.0 
+        val = -Inf ; idx = -1
 
         if status == MOI.INFEASIBLE 
             continue # Y_integer, X_integer, Gap # todo : stop, but ...
@@ -265,32 +265,8 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
             # add new sol in LBS without filtering 
             idx = push!(L.natural_order_vect, Solution(JuMP.value.(varArray), [yt_1, yt_2], curr_λ ) )
             updateCT(L.natural_order_vect.sols[idx])
-
-            # find point intersection 
-
-            # case 1 :  convexe
-            if (val < lb - 1e-4)  
-    
-                if yt_1 > ys_1+1e-4 && yt_2 > yr_2+1e-4
-                    # Y, X, G = dichoRecursion_callback(m, lp_copied, c, yr_1, yr_2, yt_1, yt_2, varArray, round_results, verbose ; args...)
-                    # append!(Y_integer, Y) ; append!(X_integer, X)
-                    # Gap += G
-                    # Y, X, G = dichoRecursion_callback(m, lp_copied, c, yt_1, yt_2, ys_1, ys_2, varArray, round_results, verbose ; args...)
-                    # append!(Y_integer, Y) ; append!(X_integer, X)
-                    # Gap += G
-                end
-
-            # case 2 : concave 
-            elseif (val > lb + 1e-4)  
-
-                # 
-
-            # case equality    
-            else 
-            end
     
         elseif status == MOI.NODE_LIMIT || status == TIME_LIMIT
-            
             if has_values(m)
                 Y, X = stock_all_primal_sols(m, f1, f2, varArray)
                 append!(Y_integer, Y) ; append!(X_integer, X)
@@ -300,7 +276,7 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
                 nothing
             else
                 best_bound = objective_bound(m)
-                ctr_bound = JuMP.@constraint(lp_copied, λ1*f1_copied + λ2*f2_copied >= best_bound)
+                ctr_bound = JuMP.@constraint(lp_copied, λ[1]*f1_copied + λ[2]*f2_copied >= best_bound)
                 JuMP.optimize!(lp_copied, ignore_optimize_hook=true)
     
                 x_star = JuMP.value.(varArray_copied)
@@ -312,140 +288,72 @@ function LBSinvokingIPsolveer(m::JuMP.Model, lp_copied::JuMP.Model, c, verbose; 
     
             yt_1 = x_star'* c[1, 2:end] + c[1, 1]
             yt_2 = x_star'* c[2, 2:end] + c[2, 1]
-            val = f1Sense == f2Sense ? λ1*yt_1 + λ2*yt_2 : λ1*yt_1 - λ2*yt_2
-    
-            if ( val < lb - 1e-4)
-                push!(vd.Y_N, round_results ? round.([yt_1, yt_2]) : [yt_1, yt_2])
-                push!(vd.X_E, x_star) ; push!(vd.lambda, curr_λ)
-    
-                if yt_1 > ys_1 +1e-4 && yt_2 > yr_2 +1e-4 
-                    Y, X, G = dichoRecursion_callback(m,lp_copied, c, yr_1, yr_2, yt_1, yt_2, varArray, round_results, verbose ; args...)
-                    append!(Y_integer, Y) ; append!(X_integer, X)
-                    Gap += G
-                    Y, X, G = dichoRecursion_callback(m, lp_copied, c, yt_1, yt_2, ys_1, ys_2, varArray, round_results, verbose ; args...)
-                    append!(Y_integer, Y) ; append!(X_integer, X)
-                    Gap += G
-                end
-            end
-    
+            val = λ[1]*yt_1 + λ[2]*yt_2
+
+            # add new sol in LBS without filtering 
+            idx = push!(L.natural_order_vect, Solution(x_star, [yt_1, yt_2], curr_λ ) )
+            updateCT(L.natural_order_vect.sols[idx])
         else
             println("has primal ? $(JuMP.has_values(m))")
             error("Condition  status $status ")
         end
-        return Y_integer, X_integer, Gap
-        
-        
+
+        # todo : make sure idx > 0
+        # -----------------------------
+        # find point intersection 
+        # -----------------------------
 
 
-    end
-
-    return Y_integer, X_integer, Gap #todo : vd.Y_N empty
-end
-
-
-
-function dichoRecursion_callback(m::JuMP.Model, lp_copied::JuMP.Model, c, λ1::Float64, λ2::Float64,
-                                L :: , 
-                                yr_1, yr_2, ys_1, ys_2, varArray, round_results, verbose ; args...)
-    global varArray
-    global x_star
-    global curr_λ
-
-
-    # Y_integer = Vector{Vector{Float64}}() ; X_integer = Vector{Vector{Float64}}()
-
-    # vd = getvOptData(m)
-    # f1, f2 = vd.objs
-    # f1Sense, f2Sense = vd.objSenses
-
-    # varArray_copied = JuMP.all_variables(lp_copied)
-
-    # f1_copied = varArray_copied'* c[1, 2:end] + c[1, 1]
-    # f2_copied = varArray_copied'* c[2, 2:end] + c[2, 1]
-
-    # f = AffExpr(0.0)
-    # Gap = 0.0
-
-    # lb = λ1*yr_1 + λ2*yr_2
-    # JuMP.set_objective(m, f1Sense, λ1*f1 + λ2*f2) ; JuMP.set_objective(lp_copied, f1Sense, λ1*f1_copied + λ2*f2_copied)
-    # verbose && println("solving for $λ1*f1 + $λ2*f2")    
-    # f = λ1*f1 + λ2*f2
-
-
-    # x_star = [] ; bst_val = -Inf 
-    # curr_λ = [λ1, λ2]
-    # JuMP.optimize!(m, ignore_optimize_hook=true) ; status = JuMP.termination_status(m)
-
-    # yt_1 = 0.0 ; yt_2 = 0.0 
-
-
-
-    if status == MOI.OPTIMAL 
-        # stock heur sol 
-        Y, X = stock_all_primal_sols(m, f1, f2, varArray)
-        append!(Y_integer, Y) ; append!(X_integer, X)
-
-        yt_1 = JuMP.value(f1) ; yt_2 = JuMP.value(f2)
-        val = f1Sense == f2Sense ? λ1*yt_1 + λ2*yt_2 : λ1*yt_1 - λ2*yt_2
-
-        if (val < lb - 1e-4)
-            push!(vd.Y_N, round_results ? round.([yt_1, yt_2]) : [yt_1, yt_2])
-            push!(vd.X_E, JuMP.value.(varArray))
-            push!(vd.lambda, curr_λv)
+        # -----------------------------
+        # case 1 :  convexe
+        # -----------------------------
+        if (val < lb - 1e-4)  
 
             if yt_1 > ys_1+1e-4 && yt_2 > yr_2+1e-4
-                Y, X, G = dichoRecursion_callback(m, lp_copied, c, yr_1, yr_2, yt_1, yt_2, varArray, round_results, verbose ; args...)
-                append!(Y_integer, Y) ; append!(X_integer, X)
-                Gap += G
-                Y, X, G = dichoRecursion_callback(m, lp_copied, c, yt_1, yt_2, ys_1, ys_2, varArray, round_results, verbose ; args...)
-                append!(Y_integer, Y) ; append!(X_integer, X)
-                Gap += G
+                # Y, X, G = dichoRecursion_callback(m, lp_copied, c, yr_1, yr_2, yt_1, yt_2, varArray, round_results, verbose ; args...)
+                # append!(Y_integer, Y) ; append!(X_integer, X)
+                # Gap += G
+                # Y, X, G = dichoRecursion_callback(m, lp_copied, c, yt_1, yt_2, ys_1, ys_2, varArray, round_results, verbose ; args...)
+                # append!(Y_integer, Y) ; append!(X_integer, X)
+                # Gap += G
             end
-        end
 
-    elseif status == MOI.NODE_LIMIT || status == TIME_LIMIT
+        # -----------------------------
+        # case 2 : concave 
+        # -----------------------------
+        elseif (val > lb + 1e-4)  
+            filtering(lb)
+
+            # add new intersection points 
+
+            # define new search direction 
+            i = idx ; j = idx 
+            while i > 0
+                i -= 1
+                if length(L.natural_order_vect.sols[i].xEquiv) > 0 break end 
+            end
+
+            while j < length(L.natural_order_vect.sols)
+                j += 1
+                if length(L.natural_order_vect.sols[j].xEquiv) > 0 break end 
+            end
+
+            # prepare next directions 
+            i ≥ 1 ? push!(todo, [L.natural_order_vect.sols[i].y, L.natural_order_vect.sols[idx].y]) : nothing 
+            j ≤ length(L.natural_order_vect.sols) ? push!(todo, [L.natural_order_vect.sols[idx].y, L.natural_order_vect.sols[j].y]) : nothing 
+
+            
+        # -----------------------------
+        # case equality    
+        # -----------------------------
+        else 
+
+        end        
         
-        if has_values(m)
-            Y, X = stock_all_primal_sols(m, f1, f2, varArray)
-            append!(Y_integer, Y) ; append!(X_integer, X)
-        end
 
-        if length(x_star) > 0
-            nothing
-        else
-            best_bound = objective_bound(m)
-            ctr_bound = JuMP.@constraint(lp_copied, λ1*f1_copied + λ2*f2_copied >= best_bound)
-            JuMP.optimize!(lp_copied, ignore_optimize_hook=true)
-
-            x_star = JuMP.value.(varArray_copied)
-
-            if JuMP.is_valid(lp_copied, ctr_bound)
-                JuMP.delete(lp_copied, ctr_bound) ; JuMP.unregister(lp_copied, :ctr_bound)
-            end
-        end
-
-        yt_1 = x_star'* c[1, 2:end] + c[1, 1]
-        yt_2 = x_star'* c[2, 2:end] + c[2, 1]
-        val = f1Sense == f2Sense ? λ1*yt_1 + λ2*yt_2 : λ1*yt_1 - λ2*yt_2
-
-        if ( val < lb - 1e-4)
-            push!(vd.Y_N, round_results ? round.([yt_1, yt_2]) : [yt_1, yt_2])
-            push!(vd.X_E, x_star) ; push!(vd.lambda, curr_λ)
-
-            if yt_1 > ys_1 +1e-4 && yt_2 > yr_2 +1e-4 
-                Y, X, G = dichoRecursion_callback(m,lp_copied, c, yr_1, yr_2, yt_1, yt_2, varArray, round_results, verbose ; args...)
-                append!(Y_integer, Y) ; append!(X_integer, X)
-                Gap += G
-                Y, X, G = dichoRecursion_callback(m, lp_copied, c, yt_1, yt_2, ys_1, ys_2, varArray, round_results, verbose ; args...)
-                append!(Y_integer, Y) ; append!(X_integer, X)
-                Gap += G
-            end
-        end
-
-    else
-        println("has primal ? $(JuMP.has_values(m))")
-        error("Condition  status $status ")
     end
-    return Y_integer, X_integer, Gap
 
+    return Y_integer, X_integer #todo : vd.Y_N empty, Gap 
 end
+
+
