57b204811614fce0d852d755679bef8a1158b423
Yue0925 <mellet1997@outlook.com>
2023-05-10 20:19:14 +0200

    testé valid !

--- experiments/instances/momhMKPstu/vOptMomkp.jl
+++ experiments/instances/momhMKPstu/vOptMomkp.jl
@@ -181,11 +181,14 @@ function main(fname::String)
 
   solverSelected = CPLEX.Optimizer
   for method in [
-    :dicho, :epsilon, 
-    # :bb, :bb_EPB,
-    :bc, :bc_EPB,
-    :bc_rootRelax , :bc_rootRelaxEPB,
-    :bc_rootRelaxCP, :bc_rootRelaxCPEPB
+    # :dicho, :epsilon, 
+    :bb, 
+    # :bb_EPB,
+    # :bc, :bc_EPB,
+    :bc_rootRelax , 
+    # :bc_rootRelaxEPB,
+    :bc_rootRelaxCP, 
+    # :bc_rootRelaxCPEPB
     ] # 
 
     vSolveBi01IP(solverSelected, dat.C, dat.A, dat.b, fname, method) 
