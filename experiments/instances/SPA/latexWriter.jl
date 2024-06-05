b13c267fcc5f83da034f8dc006cb5fe306d290bd
Yue0925 <mellet1997@outlook.com>
2024-06-05 17:25:04 +0200

    res

--- experiments/instances/SPA/latexWriter.jl
+++ experiments/instances/SPA/latexWriter.jl
@@ -1,240 +1,3 @@
-function comparisonMethods(instances::String)
-    work_dir = "../../results/" * instances
-    @assert isdir(work_dir) "This directory doesn't exist $work_dir !"
-
-    fout = open(work_dir * "/comparisonTable.tex", "w")
-
-    latex = raw"""\begin{table}[h]
-    \centering
-    \resizebox{\columnwidth}{!}{%
-    \hspace*{-1cm}\begin{tabular}{lcccccccccccc}
-    \toprule
-    \textbf{Instance} & \textbf{n} & \textbf{m} & \multicolumn{2}{c}{\textbf{Dichotomy}} & \multicolumn{2}{c}{\textbf{$\mathbf{\epsilon}$-constraint}}  & \multicolumn{3}{c}{\textbf{Branch-and-bound}} & \multicolumn{3}{c}{\textbf{Branch-and-cut}}
-    \\
-    \cmidrule(r){4-5} \cmidrule(r){6-7} \cmidrule(r){8-10} \cmidrule(r){11-13}
-    ~ & ~ & ~ & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{$|\mathcal{X}_E|$} & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{$|\mathcal{X}_E|$} \\
-    \midrule
-    """
-    println(fout, latex)
-
-    for file in readdir(work_dir * "/dicho/") # ∀ file in dicho
-        if split(file, ".")[end] != "dat"
-            continue
-        end
-
-        instName = split(file, "_")
-        if length(instName)==1
-            print(fout, file * " & ")
-        else
-            ss = ""
-            for ele in instName[1:end-1]
-                if ele != "" ss = ss * ele * "\\_" end
-            end
-            if instName[end] != "" ss = ss * instName[end] end 
-            print(fout, ss * " & ")
-        end
-        
-        times = []
-        pts = [] ; X = []
-
-        # write dichotomy result 
-        include(work_dir * "/dicho/" * file)
-        print(fout, string(vars) * " & " * string(constr) * " & ")
-        push!(times, total_times_used); push!(pts, size_Y_N)
-
-        # write ϵ-constraint result (ϵ = 0.5 by default)
-        if isfile(work_dir * "/epsilon/" * file)
-            include(work_dir * "/epsilon/" * file)
-            push!(times, total_times_used); push!(pts, size_Y_N)
-        else
-            push!(times, -1); push!(pts, -1)
-        end
-
-        # write B&B result 
-        if isfile(work_dir * "/bb/" * file)
-            include(work_dir * "/bb/" * file)
-            push!(times, total_times_used); push!(pts, size_Y_N) 
-            push!(X, size_X_E)
-        else
-            push!(times, -1); push!(pts, -1)
-            push!(X, -1)
-        end
-
-        # write B&C result 
-        if isfile(work_dir * "/bc/" * file)
-            include(work_dir * "/bc/" * file)
-            push!(times, total_times_used); push!(pts, size_Y_N) 
-            push!(X, size_X_E)
-        else
-            push!(times, -1); push!(pts, -1)
-            push!(X, -1)
-        end
-
-        for i=1:4
-            if times[i] == -1
-                print(fout, " - & ")
-            elseif times[i] == minimum(times)
-                print(fout, " \\textcolor{blue2}{" * string(times[i]) * "} & ")
-            else
-                print(fout, string(times[i]) * " & ")
-            end
-
-            if pts[i] == -1
-                print(fout, " - & ")
-            elseif pts[i] == maximum(pts)
-                print(fout, " \\textbf{" * string(pts[i]) * "} & ")
-            else
-                print(fout, string(pts[i]) * " & ")
-            end
-
-            if i==3
-                X[1]==-1 ? print(fout, " - & ") : print(fout, string(X[1]) * " & ")
-            elseif i==4
-                X[2]==-1 ? println(fout, " - \\\\") : println(fout, string(X[2]) * " \\\\")
-            end
-        end
-    end
-    latex = raw"""\bottomrule
-    \end{tabular}
-    }%"""
-    println(fout, latex)
-    println(fout, "\\caption{Comparison of the different algorithms performances for instances $instances .}")
-    println(fout, "\\label{tab:table_compare_$instances }")
-    println(fout, "\\end{table}")
-    close(fout)
-
-end
-
-
-function detailedMOBB_perform(instances::String)
-    dir = "../../results/" * instances
-    @assert isdir(dir) "This directory doesn't exist $dir !"
-
-    fout = open(dir * "/detailedMOBB.tex", "w")
-
-    latex = raw"""\begin{table}[!h]
-    \centering
-    \resizebox{\columnwidth}{!}{%
-    \hspace*{-1cm}\begin{tabular}{lccccccccccc}
-    \toprule
-    \textbf{Instance} & \textbf{n} & \textbf{m} & \multicolumn{4}{c}{\textbf{Time(s)}} & \multicolumn{2}{c}{\textbf{Nodes}}  & \textbf{Tree(MB)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{$|\mathcal{X}_E|$}
-    \\
-    \cmidrule(r){4-7} \cmidrule(r){8-9} 
-    ~ & ~ & ~ & \textbf{total} &\textbf{relax} & \textbf{dominance} & \textbf{incumbent} & \textbf{total} & \textbf{pruned} & ~ & ~ & ~ \\
-    \midrule
-    """
-    println(fout, latex)
-
-    for file in readdir(dir * "/bb/")
-        if split(file, ".")[end] != "dat"
-            continue
-        end
-
-        instName = split(file, "_")
-        if length(instName)==1
-            print(fout, file * " & ")
-        else
-            ss = ""
-            for ele in instName[1:end-1]
-                if ele != "" ss = ss * ele * "\\_" end
-            end
-            if instName[end] != "" ss = ss * instName[end] end 
-            print(fout, ss * " & ")
-        end
-
-        include(dir * "/bb/" * file)
-        print(fout, string(vars) * " & " * string(constr) * " & ")
-        print(fout, string(total_times_used)* " & " * string(relaxation_time) * " & " *
-            string(test_dominance_time) * " & " * string(update_incumbent_time) * " & " *
-            string(total_nodes) * " & " * string( pruned_nodes) * " & " * string(tree_size) * " & " *
-            string(size_Y_N) * " & " * string(size_X_E)
-        )
-
-        println(fout, "\\\\")
-    end
-
-
-    latex = raw"""\bottomrule
-    \end{tabular}%
-    }%
-    \caption{The detailed experimental information about BO01B\&B algorithm.}
-    \label{tab:table_bb}
-    \end{table}
-    """
-    println(fout, latex)
-    close(fout)
-end
-
-
-function MOBC_perform(instances::String)
-    bc = "/bc"
-    dir = "../../results/" * instances * "/" * bc
-    @assert isdir(dir) "This directory doesn't exist $dir !"
-
-    fout = open(dir * "/MOBC_bc.tex", "w")
-
-    latex = raw"""\begin{sidewaystable}[h]
-    \centering
-    \resizebox{\columnwidth}{!}{%
-    \begin{tabular}{lccccccccccccccccc}
-    \toprule
-    \textbf{Instance} & \textbf{n} & \textbf{m} & \multicolumn{2}{c}{\textbf{Nodes}} & \multicolumn{2}{c}{\textbf{CP iterations}} & \multicolumn{3}{c}{\textbf{Cuts applied}} & \textbf{Cuts} & \multicolumn{5}{c}{\textbf{CP Time(s)}} & \textbf{B\&C Time(s)} & \textbf{$|\mathcal{Y}_N|$}
-    \\
-    \cmidrule(r){4-5} \cmidrule(r){6-7} \cmidrule(r){8-10} \cmidrule(r){12-16}
-    ~ & ~ & ~ & \textbf{total} & \textbf{pruned} & \textbf{total} & \textbf{average} & \textbf{total} & \textbf{sp} & \textbf{mp} & ~ & \textbf{total} & \textbf{dichotomy} & \textbf{pool oper} & \textbf{separators} & \textbf{cuts oper} & ~ & ~ \\
-    \midrule
-    """
-    println(fout, latex)
-
-    for file in readdir(dir * "/")
-        if split(file, ".")[end] != "dat"
-            continue
-        end
-
-        instName = split(file, "_")
-        if length(instName)==1
-            print(fout, file * " & ")
-        else
-            ss = ""
-            for ele in instName[1:end-1]
-                if ele != "" ss = ss * ele * "\\_" end
-            end
-            if instName[end] != "" ss = ss * instName[end] end 
-            print(fout, ss * " & ")
-        end
-
-        include(dir * "/" * file)
-        print(fout, string(vars) * " & " * string(constr) * " & ")
-        print(fout, string(total_nodes) * " & " * string(pruned_nodes) * " & " * string(ite_total) * " & "*
-            string( round(ite_total/total_nodes, digits = 2) ) * " & " * string(cuts_applied) * " & " * string(sp_cuts)*
-            " & " * string(mp_cuts) * " & " * string(cuts_total) * " & " * string(times_total_for_cuts) * " & " *
-            string(times_calling_dicho) * " & " * string(times_oper_cutPool) * " & " *string(times_calling_separators) * " & "*
-            string(times_add_retrieve_cuts) * " & " *string(total_times_used) * " & " *string(size_Y_N)
-        )
-
-        println(fout, "\\\\")
-    end
-
-    latex = raw"""\bottomrule
-    \end{tabular}%
-    }%
-    \caption{.}
-    \label{tab:table_bc}
-    \end{sidewaystable}
-    """
-    println(fout, latex)
-    close(fout)
-
-end
-
-
-# comparisonMethods("SPA/BOSPA")
-# detailedMOBB_perform("SPA/BOSPA")
-# MOBC_perform("SPA/BOSPA")
-
-
-
-
 
 function comparisons(instances::String)
     work_dir = "../../results/" * instances
@@ -242,39 +5,95 @@ function comparisons(instances::String)
 
     fout = open(work_dir * "/comparisonTable.tex", "w")
 
-    latex = raw"""\begin{sidewaystable}[!ht]
+    latex = raw"""\begin{table}[!ht]
     \centering
     \resizebox{\columnwidth}{!}{%
-    \begin{tabular}{lcccccccccccccccccccc}
+    \begin{tabular}{lcccccccccccc}
     \toprule
-    \textbf{Instance} & \textbf{n} & \textbf{m} & \multicolumn{2}{c}{\textbf{$\mathbf{\epsilon}$-constraint}}  & \multicolumn{2}{c}{\textbf{B\&B}} & \multicolumn{2}{c}{\textbf{B\&C(LP+CP)}} & \multicolumn{2}{c}{\textbf{B\&C(cplex)}} & \multicolumn{2}{c}{\textbf{EPB B\&B}} & \multicolumn{2}{c}{\textbf{EPB B\&C(LP+CP)}} & \multicolumn{2}{c}{\textbf{EPB B\&C(cplex)}} & \multicolumn{2}{c}{\textbf{B\&C(cplex+CP)}} & \multicolumn{2}{c}{\textbf{EPB B\&C(cplex+CP)}} \\
+    \textbf{Instance} & \textbf{n} & \textbf{m} & \multicolumn{2}{c}{\textbf{$\mathbf{\epsilon}$-constraint}}  & \multicolumn{2}{c}{\textbf{B\&B}} & \multicolumn{2}{c}{\textbf{EPB B\&B}} & \multicolumn{2}{c}{\textbf{B\&C(cplex)}} & \multicolumn{2}{c}{\textbf{EPB B\&C(cplex)}} \\
     
-    \cmidrule(r){4-5} \cmidrule(r){6-7} \cmidrule(r){8-9} \cmidrule(r){10-11} \cmidrule(r){12-13} \cmidrule(r){14-15} \cmidrule(r){16-17} \cmidrule(r){18-19} \cmidrule(r){20-21}
-    ~ & ~ & ~ & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} \\
+    \cmidrule(r){4-5} \cmidrule(r){6-7} \cmidrule(r){8-9} \cmidrule(r){10-11} \cmidrule(r){12-13} 
+    
+    ~ & ~ & ~ & \textbf{Time(s)} & \textbf{$|\mathcal{Y}_N|$} & \textbf{Time(s)} & \textbf{Node} & \textbf{Time(s)} & \textbf{Node} & \textbf{Time(s)} & \textbf{Node} & \textbf{Time(s)} & \textbf{Node} \\
     \midrule
     """
     println(fout, latex)
-    methods = ["epsilon", "bb", "bc", "bc_rootRelax", "bb_EPB", "bc_EPB", "bc_rootRelaxEPB", "bc_rootRelaxCP", "bc_rootRelaxCPEPB"]
+    methods = ["epsilon", "bb", "bb_EPB", "bc_rootRelax", "bc_rootRelaxEPB"] 
 
-    # ∀ file in dicho
+
+    n = 0
+    count = 0
+    # avg_n = 0 ; avg_m = 0
+    # avgT = Dict(k => 0.0 for k in methods) ; avgY = Dict(k => 0.0 for k in methods) ; avgTO = Dict(k => 0 for k in methods)
+
+
+    # ∀ file in epsilon
     for file in readdir(work_dir * "/epsilon/")
         if split(file, ".")[end] == "png"
             continue
         end
 
-        print(fout, file * " & ")
         times = [] ; pts = []
-
-        # write dichotomy result 
         include(work_dir * "/epsilon/" * file)
+
+
+        # # new n folder 
+        # if n!= vars
+        #     if n!= 0  
+        #         avg_n = round(avg_n/count, digits = 2) ; avg_m = round(avg_m/count, digits = 2)
+        #         for m in methods
+        #             avgT[m] = round(avgT[m]/count, digits = 2); avgY[m] = round(avgY[m]/count, digits = 2) 
+        #         end
+
+        #         print(fout, "\\hline avg & " * string(avg_n) * " & " * string(avg_m) )
+
+        #         for m in methods
+        #             print(fout, " & " * string(avgT[m]) * "& " * string(avgY[m]))
+        #         end
+
+        #         println(fout, "\\\\ \\hline")
+        #         println("n = $n , count = $count TO = $avgTO")
+
+        #     end
+
+        #     n = vars 
+        #     count = 0
+        #     avg_n = 0 ; avg_m = 0
+        #     avgT = Dict(k => 0.0 for k in methods) ; avgY = Dict(k => 0.0 for k in methods) ; avgTO = Dict(k => 0 for k in methods) 
+
+        #     # count += 1
+        #     # avg_n += vars ; avg_m += constr
+        # end
+
+        count += 1
+        # avg_n += vars ; avg_m += constr
+
+        name_seg = split(file, "_")
+        for s in name_seg[1:end-1]
+            print(fout, s * "\\_")
+        end
+        print(fout, name_seg[end] * " & ")
         print(fout, string(vars) * " & " * string(constr) * " & ")
+        
 
         # ∀ method 
-        for m in methods
+        m = methods[1]
+        if isfile(work_dir * "/" * m * "/" * file)
+            include(work_dir * "/" * m * "/" * file)
+            push!(times, total_times_used); push!(pts, size_Y_N)
+            # total_times_used > 3600.0 ? avgTO[m] += 1 : nothing
+
+            # avgT[m] += total_times_used ; avgY[m] += size_Y_N
+        else
+            push!(times, -1); push!(pts, -1)
+        end
+        for m in methods[2:end]
             if isfile(work_dir * "/" * m * "/" * file)
                 include(work_dir * "/" * m * "/" * file)
-                push!(times, total_times_used); push!(pts, size_Y_N)
+                push!(times, total_times_used); push!(pts, total_nodes)
+                # total_times_used > 3600.0 ? avgTO[m] += 1 : nothing
 
+                # avgT[m] += total_times_used ; avgY[m] += total_nodes
             else
                 push!(times, -1); push!(pts, -1)
             end
@@ -296,16 +115,34 @@ function comparisons(instances::String)
 
         end
 
-        print(fout, string(times[end]) * " & ") ; println(fout, string(pts[end]) * " \\\\")
+        print(fout, string(times[end]) * " & ") 
+
+        println(fout, string(pts[end]) * " \\\\")
+
     end
+
+    # avg_n = round(avg_n/count, digits = 2) ; avg_m = round(avg_m/count, digits = 2)
+    # for m in methods
+    #     avgT[m] = round(avgT[m]/count, digits = 2); avgY[m] = round(avgY[m]/count, digits = 2) 
+    # end
+
+    # print(fout, "\\hline avg & " * string(avg_n) * " & " * string(avg_m) )
+
+    # for m in methods
+    #     print(fout, " &  " * string(avgT[m]) * " & " * string(avgY[m]))
+    # end
+
+    # println(fout, " \\\\ \\hline")
+    # println("n = $n , count = $count TO = $avgTO")
+
 
     latex = raw"""\bottomrule
     \end{tabular}
     }%"""
     println(fout, latex)
     println(fout, "\\caption{Comparison of the different algorithms performances for instances $instances .}")
-    println(fout, "\\label{tab:table_compare_$instances }")
-    println(fout, "\\end{sidewaystable}")
+    println(fout, "%\\label{tab:table_compare_$instances }")
+    println(fout, "\\end{table}")
     close(fout)
 
 end
---
+++ experiments/instances/SPA/latexWriter2.jl
@@ -0,0 +1,277 @@
+
+
+using PyPlot
+import PyPlot; const plt = PyPlot
+
+function comparisonsLambdaLimits(instances::String)
+    work_dir = "../../results/" * instances * "/lambda_limit"
+    @assert isdir(work_dir) "This directory doesn't exist $work_dir !"
+
+    fout = open(work_dir * "/comparisonLambdaLimitTable.tex", "w")
+
+    latex = raw"""\begin{sidewaystable}[!ht]
+    \centering
+    \resizebox{\columnwidth}{!}{%
+    \begin{tabular}{lccccccccccccccc}
+    \toprule
+    % line 1 
+    \textbf{Instance} & \textbf{n} & \textbf{m} & \textbf{$\mathcal{Y}_N$}
+    & \multicolumn{4}{c}{\textbf{$|\lambda| = Inf$}} & \multicolumn{4}{c}{\textbf{$|\lambda| = 2$}} & \multicolumn{4}{c}{\textbf{$|\lambda| = 3$}} \\
+    
+    %line 2 
+    \cmidrule(r){5-8} \cmidrule(r){9-12} \cmidrule(r){13-16} 
+    ~ & ~ & ~ & ~ & \multicolumn{2}{c}{\textbf{B\&C(cplex)}} & \multicolumn{2}{c}{\textbf{EPB B\&C(cplex)}} & \multicolumn{2}{c}{\textbf{B\&C(cplex)}} & \multicolumn{2}{c}{\textbf{EPB B\&C(cplex)}} & \multicolumn{2}{c}{\textbf{B\&C(cplex)}} & \multicolumn{2}{c}{\textbf{EPB B\&C(cplex)}} \\
+    
+    %line 3
+    \cmidrule(r){5-6} \cmidrule(r){7-8} \cmidrule(r){9-10} \cmidrule(r){11-12} \cmidrule(r){13-14} \cmidrule(r){15-16} 
+    ~ & ~ & ~ & ~ & \textbf{Time(s)} & \textbf{Nodes} & \textbf{Time(s)} & \textbf{Nodes} & \textbf{Time(s)} & \textbf{Nodes} & \textbf{Time(s)} & \textbf{Nodes} & \textbf{Time(s)} & \textbf{Nodes} & \textbf{Time(s)} & \textbf{Nodes} \\
+    \midrule
+    """
+    println(fout, latex)
+
+    fout2 = open(work_dir * "/comparisonLambdaLimitTable2.tex", "w")
+
+    latex = raw"""\begin{sidewaystable}[!ht]
+    \centering
+    \resizebox{\columnwidth}{!}{%
+    \begin{tabular}{lccccccccccc}
+    \toprule
+    % line 1 
+    \textbf{Instance} & \textbf{n} & \textbf{m} & \textbf{$\mathcal{Y}_N$}
+    & \multicolumn{4}{c}{\textbf{$|\lambda| = 6$}} & \multicolumn{4}{c}{\textbf{$|\lambda| = 8$}} \\
+    
+    %line 2 
+    \cmidrule(r){5-8} \cmidrule(r){9-12} 
+    ~ & ~ & ~ & ~ & \multicolumn{2}{c}{\textbf{B\&C(cplex)}} & \multicolumn{2}{c}{\textbf{EPB B\&C(cplex)}} & \multicolumn{2}{c}{\textbf{B\&C(cplex)}} & \multicolumn{2}{c}{\textbf{EPB B\&C(cplex)}} \\
+    
+    %line 3
+    \cmidrule(r){5-6} \cmidrule(r){7-8} \cmidrule(r){9-10} \cmidrule(r){11-12}
+    ~ & ~ & ~ & ~ & \textbf{Time(s)} & \textbf{Nodes} & \textbf{Time(s)} & \textbf{Nodes} & \textbf{Time(s)} & \textbf{Nodes}  & \textbf{Time(s)} & \textbf{Nodes} \\
+    \midrule
+    """
+    println(fout2, latex)
+
+
+    methods = ["bc_rootRelax", "bc_rootRelaxEPB"] #    
+
+    # method => n => (λ -> time)                     method => n => (λ -> nodes)
+    avg_time = Dict{String , Dict{Int64, Dict{Int64, Float64}}}()
+    avg_node = Dict{String , Dict{Int64, Dict{Int64, Float64}}}()
+    count_per_n = Dict{Int64, Int64}()
+
+    for m in methods
+        avg_time[m] = Dict{Int64, Dict{Int64, Float64}}()
+        avg_node[m] = Dict{Int64, Dict{Int64, Float64}}()
+    end
+
+    λ_limits = [] ;
+    for folder_λ in readdir(work_dir)
+        if split(folder_λ, ".")[end] == "tex" continue end
+        push!(λ_limits, parse(Int64 , folder_λ ) )
+    end
+    sort!(λ_limits)
+    half = round(Int64, length(λ_limits)/2 )
+
+    keys_combo = [] ; 
+    for λ in λ_limits
+        for m in methods
+            push!(keys_combo, string(λ) * "_" * m)
+        end
+    end
+
+    # println("λ_limits : ", λ_limits) ; println("keys_combo : ", keys_combo)
+
+
+    # ∀ file each line 
+    for file in readdir(work_dir * "/" * string(λ_limits[1]) * "/" * string(methods[1]) )
+        if split(file, ".")[end] == "png"
+            continue
+        end
+
+        name_seg = split(file, "_")
+        for s in name_seg[1:end-1]
+            print(fout, s * "\\_") ; print(fout2, s * "\\_")
+        end
+
+        print(fout, name_seg[end] * " & ") ; print(fout2, name_seg[end] * " & ")
+        times = [] ; pts = []
+
+        # write dichotomy result 
+        include(work_dir * "/" * string(λ_limits[1]) * "/" * string(methods[1]) * "/" * file)
+        print(fout, string(vars) * " & " * string(constr) * " & " * string(size_Y_N) * " & ")
+        print(fout2, string(vars) * " & " * string(constr) * " & " * string(size_Y_N) * " & ")
+
+        if !haskey(count_per_n, vars)
+            count_per_n[vars] = 0
+        end
+
+        for m in methods
+            if !haskey(avg_time[m], vars)
+                avg_time[m][vars] = Dict{Int64, Float64}()
+            end
+            if !haskey(avg_node[m], vars)
+                avg_node[m][vars] = Dict{Int64, Float64}()
+            end
+        end
+
+        count_per_n[vars] += 1
+
+        for folder_λ in λ_limits
+            for m in methods
+                if !haskey(avg_time[m][vars], folder_λ)
+                    avg_time[m][vars][folder_λ] = 0.0
+                end
+                if !haskey(avg_node[m][vars], folder_λ)
+                    avg_node[m][vars][folder_λ] = 0.0
+                end
+            end
+
+            for m in methods
+                if isfile(work_dir * "/" * string(folder_λ) * "/" * m * "/" * file)
+                    include(work_dir * "/" * string(folder_λ) * "/" * m * "/" * file)
+                    push!(times, total_times_used); push!(pts, total_nodes)
+
+                    avg_time[m][vars][folder_λ] += total_times_used
+                    avg_node[m][vars][folder_λ] += total_nodes
+    
+                else
+                    push!(times, -1); push!(pts, -1)
+                end
+            end
+        end
+
+
+        # each line 
+        for i=1:5
+            if times[i] == -1
+                print(fout, " - & ")
+            elseif times[i] >= 3600.0
+                print(fout, " TO & ")
+            elseif times[i] == minimum(filter(x -> x >= 0.0 ,times))
+                print(fout, " \\textcolor{blue2}{" * string(times[i]) * "} & ")
+            else
+                print(fout, string(times[i]) * " & ")
+            end
+
+            if pts[i] == -1
+                print(fout, " - & ")
+            else
+                print(fout, string(pts[i]) * " & ")
+            end
+
+        end
+
+        if times[6] == minimum(filter(x -> x >= 0.0 ,times))
+            print(fout, " \\textcolor{blue2}{" * string(times[6]) * "} & ")
+        elseif times[6] >= 3600.0
+            print(fout, " TO & ")
+        else
+            print(fout, string(times[6]) * " & ") 
+        end
+        
+        println(fout, string(pts[6]) * " \\\\")
+
+
+
+        # each line 
+        for i=7:9
+            if times[i] == -1
+                print(fout2, " - & ")
+            elseif times[i] >= 3600.0
+                print(fout2, " TO & ")
+            elseif times[i] == minimum(filter(x -> x >= 0.0 ,times))
+                print(fout2, " \\textcolor{blue2}{" * string(times[i]) * "} & ")
+            else
+                print(fout2, string(times[i]) * " & ")
+            end
+
+            if pts[i] == -1
+                print(fout2, " - & ")
+            else
+                print(fout2, string(pts[i]) * " & ")
+            end
+
+        end
+
+        if times[end] == minimum(filter(x -> x >= 0.0 ,times))
+            print(fout2, " \\textcolor{blue2}{" * string(times[end]) * "} & ")
+        elseif times[end] >= 3600.0
+            print(fout2, " TO & ")
+        else
+            print(fout2, string(times[end]) * " & ") 
+        end
+        
+        println(fout2, string(pts[end]) * " \\\\")
+
+    end
+
+
+    latex = raw"""\bottomrule
+    \end{tabular}
+    }%"""
+    println(fout, latex)
+    println(fout, "\\caption{cplex cutting LBS non-exhaustive dichotomic concave-convex like algo on instances $instances (\$\\lambda\$ fixed except EPBranched nodes) .}")
+    println(fout, "% \\label{tab:table_lambda_limits_$instances }")
+    println(fout, "\\end{sidewaystable}")
+    close(fout)
+
+    latex = raw"""\bottomrule
+    \end{tabular}
+    }%"""
+    println(fout2, latex)
+    println(fout2, "\\caption{cplex cutting LBS non-exhaustive dichotomic concave-convex like algo on instances $instances (\$\\lambda\$ fixed except EPBranched nodes) .}")
+    println(fout2, "% \\label{tab:table2_lambda_limits_$instances }")
+    println(fout2, "\\end{sidewaystable}")
+    close(fout2)
+
+
+
+    for m in methods
+        for n in keys(count_per_n)
+            for λ in λ_limits
+                avg_node[m][n][λ] = round(avg_node[m][n][λ]/count_per_n[n], digits = 2)
+                avg_time[m][n][λ] = round(avg_time[m][n][λ]/count_per_n[n], digits = 2)
+            end
+            println("$m  $n  nodes " ,  avg_node[m][n])  
+            println("$m  $n  times " ,  avg_time[m][n]) 
+
+        end
+    end
+
+    println("   --------------------   ")
+    println("λ_limits : ", λ_limits)
+    println("avg_node : ", avg_node)
+    println("avg_time : ", avg_time)
+    println("   --------------------   ")
+
+
+
+    # plot for each method, for each n 
+    for m in methods
+        for n in keys(count_per_n)
+    
+            fig, ax = plt.subplots()
+            ax.plot(λ_limits,  [p[2] for p in sort(collect(avg_time[m][n]), by = x->x[1])] ,
+                    color="red", marker="o", linewidth=2.0, linestyle="--"
+            )
+            ax.set_xlabel("|λ|", fontsize=14)
+            ax.set_ylabel("Average time(s)", color="red", fontsize=14)
+            ax.set_title("MOAP avg n = $n", fontsize=14)
+        
+            ax2=ax.twinx()
+            ax2.plot(λ_limits, [p[2] for p in sort(collect(avg_node[m][n]), by = x->x[1])], 
+                    color="blue", marker="o", linewidth=2.0, linestyle="--"
+            )
+            ax2.set_ylabel("Average explored nodes", color="blue", fontsize=14)
+            # plt.show()
+        
+            savefig(work_dir * "/$(m)_$(n)_times_nodes.png")
+            plt.close()
+
+        end
+    end
+
+end
+
+
+comparisonsLambdaLimits("SPA/BOSPA")
